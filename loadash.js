3311e8ec40e0a015edb5c9e865c682db26d5ef69
Hlib612 <glebmalonos@gmail.com>
2025-08-10 11:15:59 +0300

    add some

---
+++ loadash.js
@@ -0,0 +1,41 @@
+const tech = [
+  { label: "HTML" },
+  { label: "CSS" },
+  { label: "JavaScript" },
+  { label: "Node.js" },
+  { label: "React" },
+  { label: "Vue" },
+  { label: "Next.js" },
+  { label: "Mobx" },
+  { label: "Redux" },
+  { label: "React Router" },
+  { label: "GraphQl" },
+  { label: "PostgreSQL" },
+  { label: "MongoDB" },
+];
+
+const listElement = document.querySelector('.js-list');
+const inputElement = document.querySelector('#filter');
+console.log(listElement);
+const createListMarkup = (arr) => {
+    const markup = arr.map(item => `<li>${item.label}</li>`).join('');
+    // console.log(markup);
+    return markup;
+}
+
+
+
+const onInputChange = (event) => {
+    // console.log(event.target.value);
+    const filterValue = event.target.value.toLowerCase();
+    console.log(filterValue);
+    const filteredItems = tech.filter(item => item.label.toLowerCase().includes(filterValue));
+   console.log(filteredItems);
+listElement.innerHTML = createListMarkup(filteredItems);
+}
+
+// console.dir(_);
+
+inputElement.addEventListener('input' , _.debounce(onInputChange , 650));
+
+
